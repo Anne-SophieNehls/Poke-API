1fa8eaa79b5e892718fbc3cb6f368415551b2e50
Amme <anne-sophie.nehls@t-online.de>
2024-10-23 16:35:11 +0200

    bissel css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,18 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import { useThemeContext } from "../lib/ToggleLightDarkMode";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
+export default function Layout() {
   const { theme } = useThemeContext();
 
   return (
     <div className={`theme theme--${theme}`}>
       <Header />
       <Outlet />
-      {children}
     </div>
   );
 }
