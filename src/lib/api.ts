c5158c6be4979336e34baaa7c0ee1fd07185e737
ChrstianH <henschel_christian@web.de>
2024-10-23 11:51:34 +0200

    extended apis

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,17 @@
 export const getPokemonById = async (id: number | string) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const json = response.json();
-    return json
-}
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const json = response.json();
+  return json;
+};
+
+export const getTypeById = async (id: number | string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/ytpe/${id}`);
+  const json = response.json();
+  return json;
+};
+
+export const getAllTypes = async () => {
+  const response = await fetch(`https://pokeapi.co/api/v2/ytpe`);
+  const json = response.json();
+  return json;
+};
