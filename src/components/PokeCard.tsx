40d3e961bd6f5a38278bf5c4586fd61b924491d1
ChrstianH <henschel_christian@web.de>
2024-10-24 15:03:46 +0200

    HomePage Pokemon output

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,14 +1,17 @@
 import { Pokemon } from "../lib/Interfaces";
+import { Link } from "react-router-dom";
 
 export default function PokeCard(props: { pokemon: Pokemon }) {
   return (
     <div className="pokeCard">
       <div>
-        <img
-          src={props.pokemon.sprites.other?.home.front_default}
-          alt="pokemon-picture"
-        />
-        <div className="backgroud"></div>
+        <Link to={`/pokemon/${props.pokemon.id}`}>
+          <img
+            src={props.pokemon.sprites.other?.home.front_default}
+            alt="pokemon-picture"
+          />
+          <div className="background"></div>
+        </Link>
       </div>
       <div className="info">
         <p>{props.pokemon.id}</p>
