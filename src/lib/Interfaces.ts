1a4e6cd83ed16c33ca8b324f221bd49ae4f60ef9
ChrstianH <henschel_christian@web.de>
2024-10-24 10:16:23 +0200

    TypePage Pokemon output

--- src/lib/Interfaces.ts
+++ src/lib/Interfaces.ts
@@ -1,42 +1,65 @@
 export interface Pokemon {
-    abilities: Ability[]
-    cries: {
-    latest: string
-  }
-    height: number
-    id: number
-    location_area_encounters: string
-    name: string
-    order: number
-    stats: Stat[]
-    types: Type[]
-    weight: number
-  }
-  export interface Ability {
-    ability: {
-    name: string
-    url: string
-  }
-    is_hidden: boolean
-    slot: number
-  }
-  
-  
-  export interface Type {
-    slot: number
-    type: {
-    name: string
-    url: string
-  }
-  }
-  
-  
-  export interface Stat {
-    base_stat: number
-    effort: number
-    stat:  {
-    name: string
-    url: string
-  }
-  }
-  
+  abilities: Ability[];
+  cries: {
+    latest: string;
+  };
+  height: number;
+  id: number;
+  location_area_encounters: string;
+  name: string;
+  order: number;
+  sprites: Sprites;
+  stats: Stat[];
+  types: Type[];
+  weight: number;
+}
+export interface Ability {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface Type {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Stat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Other {
+  home: Home;
+  showdown: Sprites;
+}
+
+export interface Sprites {
+  back_default: string;
+  back_female: null;
+  back_shiny: string;
+  back_shiny_female: null;
+  front_default: string;
+  front_female: null;
+  front_shiny: string;
+  front_shiny_female: null;
+  other?: Other;
+  animated?: Sprites;
+}
+
+export interface Home {
+  front_default: string;
+  front_female: null;
+  front_shiny: string;
+  front_shiny_female: null;
+}
