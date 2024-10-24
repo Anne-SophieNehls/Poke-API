bd4d481b3b8e9990846cdf81fedc1a8405c556a8
Amme <anne-sophie.nehls@t-online.de>
2024-10-24 17:58:09 +0200

    kein bock mehr

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,31 +7,45 @@ export default function Header() {
 
   return (
     <header>
-      <section className="p-menu1">
-        <nav id="navbar" className="navigation" role="navigation">
-          <input id="toggle1" type="checkbox" />
-          <label className="hamburger1" htmlFor="toggle1">
-            <div className="top"></div>
-            <div className="meat"></div>
-            <div className="bottom"></div>
-          </label>
-          <nav className="menu1">
-            <NavLink className="link1" to="/">
-              Home
-            </NavLink>
-            <NavLink className="link1" to="/type">
-              Types
-            </NavLink>
-            <NavLink className="link1" to="/about-us">
-              About us
-            </NavLink>
+      <div className="logo-container">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+          alt="Logo-Pokemon"
+        />
+        <h1>Gotta Fetch 'Em All</h1>
+      </div>
+      <div className="elemtene-header">
+        <section className="p-menu1">
+          <nav id="navbar" className="navigation" role="navigation">
+            <input id="toggle1" type="checkbox" />
+            <label className="hamburger1" htmlFor="toggle1">
+              <div className="top"></div>
+              <div className="meat"></div>
+              <div className="bottom"></div>
+            </label>
+            <nav className="menu1">
+              <NavLink className="link1" to="/">
+                Home
+              </NavLink>
+              <NavLink className="link1" to="/type">
+                Types
+              </NavLink>
+              <NavLink className="link1" to="/about-us">
+                About us
+              </NavLink>
+            </nav>
           </nav>
-        </nav>
-      </section>
-      <form>
-        <input type="text" placeholder="Search Pokemon" ref={searchRef} />
-      </form>
-      <ThemeSwitcher />
+        </section>
+        <form>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search Pokemon"
+            ref={searchRef}
+          />
+        </form>
+        <ThemeSwitcher />
+      </div>
     </header>
   );
 }
